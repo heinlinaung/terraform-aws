bb58b49575950789e6398f562cf002f6bb4f0ba5
Hein Lin Aung <heinlinaung6891@gmail.com>
2024-04-14 19:42:41 +0700

    Added : tf for multi-region deployment

---
+++ tf-multi-region/terraform.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  alias = "virginia"
+}
+
+provider "aws" {
+  region = "us-west-2"
+  alias = "oregon"
+}
